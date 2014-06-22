a105fb50591d65f58a21587b6b536389309ad4e0
gubjr <gubjanos@gmail.com>
2014-06-22 13:33:23 +0200

    Refactored classify.py: train and test paths now constants

--- classify.py
+++ classify.py
@@ -1,10 +1,12 @@
 # Constants
 NUMBER_OF_COMPONENTS = 200 # the remaining number of components after dimension reduction
+TRAIN_PATH = "c:\\gravity\\valami_train_no_duplicates.txt"
+TEST_PATH = "c:\\gravity\\valami_test.txt"
 
 from load_data import load_data
 print("Loading data...")
-x, y = load_data("c:\\gravity\\valami_train_no_duplicates.txt")
-x_test, y_test = load_data("c:\\gravity\\valami_test.txt")
+x, y = load_data(TRAIN_PATH)
+x_test, y_test = load_data(TEST_PATH)
 
 print("Scaling...")
 from transforming import scale
